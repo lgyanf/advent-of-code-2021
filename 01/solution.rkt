9c10177cbd67ac92eb692f6b2d189a7662ecf5ed
Ivan Gromov <m@ivangromov.me>
2021-12-01 14:18:39 +0000

    01 Dec/2

--- 01/solution.rkt
+++ 01/solution.rkt
@@ -2,6 +2,11 @@
 
 (require racket/sequence)
 
+(define (read-numbers filename)
+  (with-input-from-file filename
+    (lambda ()
+      (map string->number (sequence->list (in-lines))))))
+
 (define (count-decreases depths)
   (define (iter depths last-depth acc)
     (if (null? depths)
@@ -10,9 +15,18 @@
           (iter (cdr depths) depth (if (> depth last-depth) (+ acc 1) acc)))))
   (if (null? depths) 0 (iter (cdr depths) (car depths) 0)))
 
-(define (count-decreases-from-file filename)
-  (with-input-from-file filename
-    (lambda ()
-      (count-decreases (map string->number (sequence->list (in-lines)))))))
+(define (sliding-window-3-sum nums)
+  (define (iter nums a b c acc)
+    (if (null? nums)
+        acc
+        (iter b c (car nums) (append acc (list (+ a b c))))))
+  (iter (cddr nums) (car nums) (cadr nums) (caddr nums) '()))
 
-(count-decreases-from-file "input.txt")
+(define (input) (read-numbers "input.txt"))
+
+(display "Task 1: ")
+(display (count-decreases (input)))
+(newline)
+(sliding-window-3-sum (input))
+
+(display "Task 2: ")
