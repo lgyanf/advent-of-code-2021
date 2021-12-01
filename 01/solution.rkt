724f65034439bec9d8e119fc1eb7b956e29867f0
Ivan Gromov <m@ivangromov.me>
2021-12-01 14:37:30 +0000

    Rewrite sliding window to vector

--- 01/solution.rkt
+++ 01/solution.rkt
@@ -1,6 +1,7 @@
 #lang racket/base
 
 (require racket/sequence)
+(require racket/list)
 
 (define (read-numbers filename)
   (with-input-from-file filename
@@ -15,18 +16,23 @@
           (iter (cdr depths) depth (if (> depth last-depth) (+ acc 1) acc)))))
   (if (null? depths) 0 (iter (cdr depths) (car depths) 0)))
 
+(define nil '())
+
 (define (sliding-window-3-sum nums)
-  (define (iter nums a b c acc)
-    (if (null? nums)
-        acc
-        (iter b c (car nums) (append acc (list (+ a b c))))))
-  (iter (cddr nums) (car nums) (cadr nums) (caddr nums) '()))
+  (let ((result (make-vector (- (length nums) 2))))
+    (define (iter nums a b c position)
+      (vector-set! result position (+ a b c))
+      (if (null? nums)
+        (vector->list result)
+        (iter (cdr nums) b c (car nums) (+ position 1))))
+    (iter (cdddr nums) (car nums) (cadr nums) (caddr nums) 0)))
 
 (define (input) (read-numbers "input.txt"))
 
 (display "Task 1: ")
 (display (count-decreases (input)))
 (newline)
-(sliding-window-3-sum (input))
 
 (display "Task 2: ")
+(display (count-decreases (sliding-window-3-sum (input))))
+(newline)
